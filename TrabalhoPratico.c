ea60fb660304b45fe642425ba40d0c525d7bfabc
somat27 <tomasmeadela@gmail.com>
2022-11-30 21:31:39 +0000

    V1.0.1
    Adicionado um comando que serve para limpar a tele do user, pode ser útil em muitas funções

--- TrabalhoPratico.c
+++ TrabalhoPratico.c
@@ -2,14 +2,6 @@
 #include <time.h>
 #include <locale.h> 
 
-struct login //Estrutura de login
-{
-    char fname[30];
-    char lname[30];
-    char username[30];
-    char password[20];
-};
-
 void interface();
 int loginRegister();
 void loginConta();
@@ -68,78 +60,9 @@ int loginRegister(){
 }
 
 void loginConta(){
-	char username[30],password[20];
-    FILE *log;
-
-    log = fopen("login.txt","r");
-    if (log == NULL)
-    {
-        fputs("Error at opening File!", stderr);
-        fopen("login.txt", "wb");
-    }
-
-    struct login l;
-
-    printf("\nPlease Enter your login credentials below\n\n");
-    printf("Username:  ");
-    fgets(username, 30, stdin);
-    printf("\nPassword: ");
-    printf("\n");
-    fgets(password, 20, stdin);
-
-    while(fread(&l,sizeof(l),1,log))
-        {
-        if(strcmp(username,l.username)==0 && strcmp(password,l.password)==0)
-
-            {   
-                printf("\nSuccessful Login\n");
-            }
-        else 
-            {
-                printf("\nIncorrect Login Details\nPlease enter the correct credentials\n");
-            }
-        }
-
-    fclose(log);
-
-    return;
+	
 }
 
 void registerConta(){
-	char firstname[15];
-    FILE *log;
-
-    log=fopen("login.txt","w");
-    if (log == NULL)
-    {
-        fputs("Error at opening File!", stderr);
-        fopen("login.txt", "wb");
-    }
-
-
-    struct login l;
-
-    printf("\nWelcome to your online course provider. We need to enter some details for registration.\n\n");
-    printf("\nEnter First Name:\n");
-    scanf("%c",l.fname);
-    printf("\nEnter Surname:\n");
-    scanf("%s",l.lname);
-
-    printf("Thank you.\nNow please choose a username and password as credentials for system login.\nEnsure the username is no more than 30 characters long.\nEnsure your password is at least 8 characters long and contains lowercase, uppercase, numerical and special character values.\n"); 
-
-    printf("\nEnter Username:\n");
-    scanf("%s",l.username);
-    printf("\nEnter Password:\n");
-    scanf("%s",l.password);
-
-
-    fwrite(&l,sizeof(l),1,log);
-    fclose(log);
-
-    printf("\nConfirming details...\n...\nWelcome, %s!\n\n",firstname);
-    printf("\nRegistration Successful!\n");
-    printf("Press any key to continue...");
-        getchar();
-    system("CLS");
-    login();
+	
 }
